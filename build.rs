3e41f890b1e3197e9d85ea2561b6b4495e2547ea
Billie Thompson <billie@billiecodes.com>
2020-04-19 11:37:57 +0200

    Set name automatically
    This sets the app name automatically as part of the build. The intention
    being to make this build bit more copyable.

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
 use std::env;
 
 fn main() {
-    let app_name = "game-of-life";
+    let current_dir = env::current_dir().unwrap();
+    let default_app_name = current_dir.as_path().file_name().unwrap().to_str().unwrap();
+    let app_name = env::var("REPOSITORY_NAME").unwrap_or_else(|_| default_app_name.to_string());
     println!("cargo:rustc-env=APP_NAME={}", app_name);
     println!(
         "cargo:rustc-env=AUTHOR_EMAIL=Billie Thompson <billie+{}@purplebooth.co.uk>",
         app_name
     );
-    println!(
-        "cargo:rustc-env=VERSION={}",
-        env::var("VERSION").unwrap_or_else(|_| "dev".to_string())
-    );
+    let version = env::var("VERSION").unwrap_or_else(|_| "dev".to_string());
+    println!("cargo:rustc-env=VERSION={}", version);
 }
